8e0235ce8c62165795ad1652a35f80903d88ceda
MSarfarazH <m.s.hussain@outlook.com>
2020-09-23 17:28:26 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def index
+        #@dog = Dog.all(sort_by :employees)
+        @dogs = Dog.billy
+    end
 end
+
