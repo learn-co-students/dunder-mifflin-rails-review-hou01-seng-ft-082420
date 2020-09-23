9ded0ef1c438527e952a7996f94ffd372eed5e8a
stephenandersondev <stephenandersondev@gmail.com>
2020-09-23 14:32:19 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all.group(:owners).count
+        byebug
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+
 end
